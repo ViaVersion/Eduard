81efaaef83d1bd15b09aa83c9c22829094a63fbb
KennyTV <kennytv@t-online.de>
2020-06-24 17:13:31 +0200

    Fix NPE

--- src/main/java/eu/kennytv/viaeduard/listener/HelpMessageListener.java
+++ src/main/java/eu/kennytv/viaeduard/listener/HelpMessageListener.java
@@ -4,6 +4,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import eu.kennytv.viaeduard.ViaEduardBot;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -23,11 +24,14 @@ public final class HelpMessageListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(@NotNull final GuildMessageReceivedEvent event) {
+        final Member member = event.getMember();
+        if (member == null) return; // Webhook
+
         final long id = event.getAuthor().getIdLong();
         if (id == bot.getJda().getSelfUser().getIdLong()) return;
         if (recentlySent.getIfPresent(id) != null) return;
-        // Exempt people with nick manage perm
-        if (event.getMember().hasPermission(Permission.VOICE_MOVE_OTHERS)) return;
+        // Exempt people with move perm
+        if (member.hasPermission(Permission.VOICE_MOVE_OTHERS)) return;
 
         final Message message = event.getMessage();
         if (!message.getContentRaw().toLowerCase().contains("help")) return;

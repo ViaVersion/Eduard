e30aeb1f4e922f51196659a0a0b517ee35a216c6
Nassim Jahnke <nassim@njahnke.dev>
2024-08-08 10:53:11 +0200

    Handle ci skip commits in git commit check

--- src/main/java/com/viaversion/eduard/util/GitVersionUtil.java
+++ src/main/java/com/viaversion/eduard/util/GitVersionUtil.java
@@ -2,42 +2,45 @@ package com.viaversion.eduard.util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public final class GitVersionUtil {
+    private static final String COMPARE_URL = "https://api.github.com/repos/%s/compare/%s...%s";
+    private static final Gson GSON = new Gson();
 
     // Yoinked from PaperMC by Techcable <Techcable@outlook.com>
-    public static int fetchDistanceFromGitHub(final String repo, final String branch, final String hash) {
+    public static int fetchDistanceFromGitHub(final String repo, final String expectedCommit, final String givenCommit) {
         HttpURLConnection connection = null;
+        final String parentCommit;
         try {
-            connection = (HttpURLConnection) new URL("https://api.github.com/repos/" + repo + "/compare/" + branch + "..." + hash).openConnection();
+            connection = (HttpURLConnection) new URL(String.format(COMPARE_URL, repo, expectedCommit, givenCommit)).openConnection();
             connection.connect();
             if (connection.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
                 return -2; // Unknown commit
             }
 
             try (final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                final JsonObject obj = new Gson().fromJson(reader, JsonObject.class);
+                final JsonObject obj = GSON.fromJson(reader, JsonObject.class);
                 final String status = obj.get("status").getAsString();
-                switch (status) {
-                    case "identical":
-                        return 0;
-                    case "behind":
-                        return obj.get("behind_by").getAsInt();
-                    default:
-                        return -1;
+                final JsonObject mostRecentCommit = obj.getAsJsonObject("base_commit");
+                final String commitMessage = mostRecentCommit.getAsJsonObject("commit").getAsJsonPrimitive("message").getAsString();
+                if (status.equals("identical")) {
+                    return 0;
                 }
-            } catch (final JsonSyntaxException | NumberFormatException e) {
-                e.printStackTrace();
-                return -1;
+
+                if (!commitMessage.startsWith("[ci skip]")) {
+                    return status.equals("behind") ? obj.get("behind_by").getAsInt() : -1;
+                }
+
+                // Fall through to fetch the parent commit after connection and reader are closed
+                final JsonObject parent = mostRecentCommit.getAsJsonArray("parents").get(0).getAsJsonObject();
+                parentCommit = parent.getAsJsonPrimitive("sha").getAsString();
             }
-        } catch (final IOException e) {
+        } catch (final Exception e) {
             e.printStackTrace();
             return -1;
         } finally {
@@ -45,5 +48,8 @@ public final class GitVersionUtil {
                 connection.disconnect();
             }
         }
+
+        // ci-skipped commit, try again with parent commit hash
+        return fetchDistanceFromGitHub(repo, parentCommit, givenCommit);
     }
 }

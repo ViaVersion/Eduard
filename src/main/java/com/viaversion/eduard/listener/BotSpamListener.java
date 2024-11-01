653b92549db9d069619a45f28a0515d5ce4681a7
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2024-11-01 19:11:50 +0100

    Delete messages in bot-spam after 5 min (except from staff) (#109)

---
+++ src/main/java/com/viaversion/eduard/listener/BotSpamListener.java
@@ -0,0 +1,29 @@
+package com.viaversion.eduard.listener;
+
+import com.viaversion.eduard.ViaEduardBot;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import java.util.concurrent.TimeUnit;
+
+public class BotSpamListener extends ListenerAdapter {
+    private final ViaEduardBot bot;
+
+    public BotSpamListener(final ViaEduardBot bot) {
+        this.bot = bot;
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull final MessageReceivedEvent event) {
+        if (!event.isFromGuild() || !event.isFromType(ChannelType.TEXT) || event.getGuildChannel().getIdLong() != bot.getBotSpamChannelId()) {
+            return;
+        }
+
+        if (event.getMember().getRoles().stream().anyMatch(role -> role.getIdLong() == bot.getStaffRoleId())) {
+            return;
+        }
+
+        event.getMessage().delete().queueAfter(5, TimeUnit.MINUTES);
+    }
+}

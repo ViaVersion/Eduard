f86ad0a1b79dd15f3d06a3bb377b3bac954a1b11
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2024-11-03 11:25:41 +0100

    Allow Athena in bot-spam (#110)

--- src/main/java/com/viaversion/eduard/listener/LogListener.java
+++ src/main/java/com/viaversion/eduard/listener/LogListener.java
@@ -42,7 +42,7 @@ public final class LogListener extends ListenerAdapter {
         }
 
         long channelId = event.getGuildChannel().getIdLong();
-        if (channelId != bot.getPluginSupportChannelId() && channelId != bot.getBotChannelId()) {
+        if (channelId != bot.getPluginSupportChannelId() && channelId != bot.getBotChannelId() && channelId != bot.getBotSpamChannelId()) {
             return;
         }
 

49928a467d9037c54c6eccbe7d6e648d6f97c3c6
FlorianMichael <florian.michael07@gmail.com>
2025-07-06 17:51:35 +0200

    Search for a free name if channel thread already exists;
    Also extend both lists

--- src/main/java/com/viaversion/eduard/util/StringUtil.java
+++ src/main/java/com/viaversion/eduard/util/StringUtil.java
@@ -1,59 +1,109 @@
 package com.viaversion.eduard.util;
 
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+
 import java.util.Random;
 
 // Thanks to https://github.com/e-im/pencil/blob/main/src/main/java/me/sulu/pencil/util/StringUtil.java
 public final class StringUtil {
 
     private static final String[] adjectives = {
-        "Fast",
-        "Quick",
-        "Slow",
-        "Bright",
-        "Noisy",
-        "Loud",
-        "Quiet",
-        "Brave",
-        "Sad",
-        "Proud",
-        "Happy",
-        "Comfortable",
-        "Clever",
-        "Interesting",
-        "Funny",
-        "Kind",
-        "Polite",
-        "Fair",
-        "Careful",
-        "Safe",
-        "Dangerous"
+            "Fast",
+            "Quick",
+            "Slow",
+            "Bright",
+            "Noisy",
+            "Loud",
+            "Quiet",
+            "Brave",
+            "Sad",
+            "Proud",
+            "Happy",
+            "Comfortable",
+            "Clever",
+            "Interesting",
+            "Funny",
+            "Kind",
+            "Polite",
+            "Fair",
+            "Careful",
+            "Safe",
+            "Dangerous",
+            "Curious",
+            "Sneaky",
+            "Gentle",
+            "Bold",
+            "Lazy",
+            "Energetic",
+            "Playful",
+            "Fierce",
+            "Mighty",
+            "Tiny",
+            "Giant",
+            "Grumpy",
+            "Shy",
+            "Witty",
+            "Charming",
+            "Angry",
+            "Sleepy",
+            "Loyal",
+            "Elegant",
+            "Swift"
     };
     private static final String[] animals = {
-        "Aardvark",
-        "Alligator",
-        "Ant",
-        "Crab",
-        "Cricket",
-        "Crow",
-        "Deer",
-        "Dog",
-        "Flamingo",
-        "Frog",
-        "Fox",
-        "Herring",
-        "Llama",
-        "Mongoose",
-        "Quail",
-        "Tiger",
-        "Weasel",
-        "Wolf",
-        "Yak",
-        "Zebra"
+            "Aardvark",
+            "Alligator",
+            "Ant",
+            "Crab",
+            "Cricket",
+            "Crow",
+            "Deer",
+            "Dog",
+            "Flamingo",
+            "Frog",
+            "Fox",
+            "Herring",
+            "Llama",
+            "Mongoose",
+            "Quail",
+            "Tiger",
+            "Weasel",
+            "Wolf",
+            "Yak",
+            "Zebra",
+            "Bat",
+            "Cheetah",
+            "Dolphin",
+            "Eagle",
+            "Elephant",
+            "Giraffe",
+            "Hamster",
+            "Iguana",
+            "Jaguar",
+            "Kangaroo",
+            "Leopard",
+            "Moose",
+            "Narwhal",
+            "Ocelot",
+            "Ostrich",
+            "Panda",
+            "Penguin",
+            "Raccoon",
+            "Seal",
+            "Walrus"
     };
 
     private static final Random random = new Random();
 
-    public static String randomName() {
-        return adjectives[random.nextInt(adjectives.length)] + animals[random.nextInt(animals.length)];
+    public static String randomName(final TextChannel channel) {
+        final String name = adjectives[random.nextInt(adjectives.length)] + animals[random.nextInt(animals.length)];
+        for (final ThreadChannel threadChannel : channel.getThreadChannels()) {
+            if (threadChannel.getName().equalsIgnoreCase(name)) {
+                return randomName(channel);
+            }
+        }
+
+        return name;
     }
 }
